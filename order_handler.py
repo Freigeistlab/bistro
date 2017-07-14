4a16a91eee9f2a63814fc3ec066a8aa2db12b009
Andreas Rau <andreas.rau@student.hpi.uni-potsdam.de>
2017-07-14 13:00:55 +0200

    figured out correct encoding

--- order_handler.py
+++ order_handler.py
@@ -11,7 +11,7 @@ class OrderHandler(threading.Thread):
 		host = '' # meaning all available interfaces on our machine
 		port = 9100 # as used by orderbird
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # makes it easier to restart the program
 		self.socket.bind((host, port))
 		self.socket.listen(1)
 
@@ -23,12 +23,12 @@ class OrderHandler(threading.Thread):
 
 				while True:
 					print("Receiving data...")
-					data = conn.recv(8192).decode("latin-1")
+					data = conn.recv(8192).decode("cp1252")
 
 					if not data:
 						break
 
-					elif data == bytes([16, 4, 1, 16, 4, 2, 16, 4, 3, 16, 4, 4]).decode("latin-1"):
+					elif data == bytes([16, 4, 1, 16, 4, 2, 16, 4, 3, 16, 4, 4]).decode("cp1252"):
 						conn.send(bytes([22, 18, 18, 18]))
 						print("send check")
 
@@ -38,7 +38,28 @@ class OrderHandler(threading.Thread):
 						conn.send(bytes(liste))
 
 						print("=== New order ===")
+						print(data)
 
+						regex_total = re.compile("Total(\s|\c)*[\d]+,[\d]{2}")
+						totals = regex_total.findall(data)
+
+						regex_id = re.compile("(Rechnung Nr\.[\d]?-?[\d]*)")
+						ids = regex_id.findall(data)
+
+						regex_date = re.compile("(([\d][\d]).([\d][\d]).([\d][\d][\d][\d]))")
+						dates = regex_date.findall(data)
+
+						regex_time = re.compile("(([\d][\d][:][\d][\d]))")
+						times = regex_time.findall(data)
+
+						regex_item = re.compile("(([\d])x *([A-Za-z]* .*?)([\d]*,[\d]*) € *([\d]*,[\d]*) €)")
+						items = regex_item.findall(data)
+
+						print("Totals: ", totals)
+						print("IDs: ", ids)
+						print("Dates: ", dates)
+						print("Times: ", times)
+						print("Items: ", items)
 
 
 			except:
