0a60a6c2afeb427658e6cbc4cb7769661402bc59
Andreas Rau <andreas.rau@student.hpi.uni-potsdam.de>
2018-05-27 17:36:46 +0200

    fixed regexes for printer control chars

--- order_handler.py
+++ order_handler.py
@@ -78,55 +78,80 @@ class OrderHandler(threading.Thread):
 						conn.send(bytes(liste))
 
 						if self.fakeData:
-							data = """Bon            26 Tischbon
-Datum          14:12 31.03.18
-Bedient von    Armin Admin 1234
-Terminal       iPhone von Michael
-Tisch          Theke
+							data = bytes([10, 27, 116, 16, 27, 82, 0, 10, 27, 97, 0, 66, 111, 110, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 29, 33, 17, 27, 69, 1, 50, 54, 27, 69, 0, 29, 33, 0, 32, 84, 105, 115, 99, 104, 98, 111, 110, 10, 27, 97, 0, 68, 97, 116, 117, 109, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 48, 49, 58, 52, 55, 32, 50, 53, 46, 48, 53, 46, 49, 56, 10, 27, 97, 0, 66, 101, 100, 105, 101, 110, 116, 32, 118, 111, 110, 32, 32, 32, 32, 65, 114, 109, 105, 110, 32, 65, 100, 109, 105, 110, 32, 49, 50, 51, 52, 10, 27, 97, 0, 84, 101, 114, 109, 105, 110, 97, 108, 32, 32, 32, 32, 32, 32, 32, 105, 80, 104, 111, 110, 101, 32, 118, 111, 110, 32, 77, 105, 99, 104, 97, 101, 108, 10, 27, 97, 0, 84, 105, 115, 99, 104, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 29, 33, 17, 84, 105, 115, 99, 104, 32, 49, 29, 33, 0, 10, 27, 97, 0, 29, 33, 0, 10, 10, 10, 10, 27, 97, 0,
+45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45,
+45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 10, 27, 97, 0, 29, 33, 17,
+84, 97, 103, 108, 105, 97, 116, 101, 108, 108, 101,
+29, 33, 0,
+10,
+29, 33, 17,
+32, 32, 32, 65, 114, 114, 97, 98, 105, 97, 116, 97,
+29, 33, 0,
+10,
+29,
+33, 17,
+32, 32, 32, 80, 97, 114, 109, 101, 115, 97, 110,
+29, 33, 0,
+10,
+27, 97, 0,
+32, 32, 32, 32, 32, 32,
+32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 40, 53, 44,
+48, 48, 41, 32, 53, 44, 48, 48,
+10,
+45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45,
+45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45,
+29, 40, 72, 6, 0, 48, 48, 51, 54, 51, 50, 29, 86, 66, 10]).decode("cp1252")
+# 							data = """Bon            26 Tischbon
+# Datum          14:12 31.03.18
+# Bedient von    Armin Admin 1234
+# Terminal       iPhone von Michael
+# Tisch          Theke
 
-------------------------------------------
-Penne
-   Bolognese
-   Mais
-   Hackfleisch
-   Ohne Tomaten
-   Ohne Parmesan
-   Coca-Cola
-                            (20,90) 20,90
-------------------------------------------
-Farfalle
-    Arrabiata
-                            (20,90) 20,90
-------------------------------------------
-Penne
-    Bolognese
-                            (20,90) 20,90
-------------------------------------------
-3x Tagliatelle
-   Bolognese
-   Mais
-   Ohne Hackfleisch
-   Ohne Tomaten
-   Ohne Parmesan
-                            (20,90) 20,90
-------------------------------------------
-2x Penne
-   Arrabiata
-   Parmesan
-   Salzkartoffeln
-   Ohne Chili
-   Fanta
-                            (23,50) 23,50
-------------------------------------------
-"""
+# ------------------------------------------
+# Penne
+#    Bolognese
+#    Mais
+#    Hackfleisch
+#    Ohne Tomaten
+#    Ohne Parmesan
+#    Coca-Cola
+#                             (20,90) 20,90
+# ------------------------------------------
+# Farfalle
+#     Arrabiata
+#                             (20,90) 20,90
+# ------------------------------------------
+# Penne
+#     Bolognese
+#                             (20,90) 20,90
+# ------------------------------------------
+# 3x Tagliatelle
+#    Bolognese
+#    Mais
+#    Ohne Hackfleisch
+#    Ohne Tomaten
+#    Ohne Parmesan
+#                             (20,90) 20,90
+# ------------------------------------------
+# 2x Penne
+#    Arrabiata
+#    Parmesan
+#    Salzkartoffeln
+#    Ohne Chili
+#    Fanta
+#                             (23,50) 23,50
+# ------------------------------------------
+# """
 
 						print("  New order:")
 						if self.verbose:
 							print(data)
 
-						orders = re.compile("(?<=-{42}\s)[\w\s-]+").findall(data)
+						orders = re.compile("(?<=-{42}\s\x1ba\x00\x1d!\x11)[\s\S]+?(?=\x1d!\x00\n\x1ba\x00\s+\(\d+,\d{2}\)\s\d+,\d{2}\s-{42})").findall(data)
+						#orders = re.compile("(?<=-{42}\s)[\w\s-]+").findall(data)
 
-						print("    Orders: ", [o.strip() for o in orders])
+						if self.verbose:
+							print("    Orders: ", [o.strip() for o in orders])
 
 						for order in orders:
 							# remove unnecessary whitespaces
@@ -141,11 +166,14 @@ Penne
 							else:
 								amount = 1
 
-							items = re.split('\n', order)
+							items = re.split('\x1d!\x00\n\x1d!\x11', order)
 
 							for index, item in enumerate(items):
-								# remove unnecessary whitespaces
-								items[index] = item.strip()
+								# remove unnecessary whitespaces and control strings
+								items[index] = items[index].strip()
+
+							if self.verbose:
+								print("    Items: ", items)
 
 							dishName = items[0] + " " + items[1]
 							extras = ""
