460d4985eac7cbfc2730d4a8fccc2746e412e1aa
Nico Böckhoff <nico.boeckhoff@student.hpi.de>
2018-11-19 21:05:35 +0100

    Feature/startup script (#4)
    * added startup script
    
    * added startup script
    
    * fixed typo

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Freigeist
+# script to be run at boot of docker container
+# after running this, open:
+# http://0.0.0.0:8000/src/frontprojection/bistro.html for frontprojection
+# http://0.0.0.0:8000/src/backprojection/bistro_back.html for backprojection
+
+git pull origin master
+python3 -m http.server 8000 &
+python3 src/server/bistro.py
